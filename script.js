ed797d1865063e489b2e6d5d82ce5e273484a776
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-16 17:03:58 +0100

    Phase 1 Implementation
    Achieved stated objectives using eventlisteners

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // Scripts needed to implement etch-a-sketch
 
+const container = document.querySelector('#container');
+
+for(let i = 1; i<=16; i++){
+
+    for(let k = 1; k<=16; k++){
+
+        const divList = document.createElement('div');
+
+        divList.classList.add("box");
+
+        container.appendChild(divList);
+    }
+}
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+
+    // and for each one we add a 'click' listener
+    box.addEventListener('mouseover', () => {
+      
+    box.classList.add("shadeBox");
+     
+    });
+  });
