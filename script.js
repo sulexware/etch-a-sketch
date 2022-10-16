e719eaaa6c113a9a74c9bba58a58b8a7010969e7
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-16 19:04:41 +0100

    Extra Credit
    Achieved extra credit objectives.

--- script.js
+++ script.js
@@ -1,27 +1,77 @@
 // Scripts needed to implement etch-a-sketch
-
+let sketchSize = 16;
+let alphaColor = 0;
+let aCount = 0;
 const container = document.querySelector('#container');
 
-for(let i = 1; i<=16; i++){
+generateSketch(sketchSize);
 
-    for(let k = 1; k<=16; k++){
+const btn = document.querySelector('#btnSize');
 
-        const divList = document.createElement('div');
+btn.addEventListener('click', () => {
+      
+    // get sketch size
 
-        divList.classList.add("box");
+    sketchSize = prompt("Please enter a value between 0 and 100.");
 
-        container.appendChild(divList);
+    generateSketch(sketchSize);
+     
+});
+
+function generateSketch(skSize) {
+    // erase previous table
+
+    container.innerHTML ="";
+
+    // generate a new sketch table based on size
+
+    for(let i = 1; i<=skSize; i++){
+
+        for(let k = 1; k<=skSize; k++){
+    
+            const divList = document.createElement('div');
+    
+            divList.classList.add("box");
+    
+            container.appendChild(divList);
+        }
     }
+
+    let boxes = document.querySelectorAll('.box');
+
+    boxes.forEach((box) => {
+    
+        box.addEventListener('mouseover', () => {
+          
+       // box.classList.add("shadeBox");
+
+            // Count
+
+            aCount++;
+
+            alphaColor = alphaColor + 0.1;
+
+            // Random colors
+
+            let rColor = Math.floor(Math.random() * 255 );
+            let gColor = Math.floor(Math.random() * 255 );
+            let bColor = Math.floor(Math.random() * 255 );
+
+            let rgb = 'rgba(' + rColor + ',' + gColor + ',' + rColor + ',' + alphaColor + ')';
+
+            box.style.backgroundColor = rgb;
+
+            // reset counter
+
+            if(aCount >= 10){
+                aCount = 0;
+                alphaColor =0;
+            }
+         
+        });
+    });
 }
 
-const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
 
-    // and for each one we add a 'click' listener
-    box.addEventListener('mouseover', () => {
-      
-    box.classList.add("shadeBox");
-     
-    });
-  });
+  
